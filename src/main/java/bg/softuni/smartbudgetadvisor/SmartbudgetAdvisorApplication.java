6c35bc4333b832ad394f45806a37911a99948b62
MartinBST <martin.kanev@bst.bg>
2025-03-01 21:10:27 +0200

    app structure

---
+++ src/main/java/bg/softuni/smartbudgetadvisor/SmartbudgetAdvisorApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.smartbudgetadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbudgetAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartbudgetAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/smartbudgetadvisor/SmartbudgetAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.smartbudgetadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbudgetAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
